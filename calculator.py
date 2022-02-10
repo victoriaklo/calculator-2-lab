e203ded46be75a3988a6e175085feb6d98ab6d12
Victoria <victoriakarenlo@gmail.com>
2022-02-10 00:33:17 +0000

    calculator running

--- calculator.py
+++ calculator.py
@@ -2,7 +2,10 @@
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
-
+#print explicit description of how to notate the operations
+#possible_operations = ['+', '-', 'pow', etc...]
+#if tokens[0] not in possible_operations:
+    #try again
 
 # Replace this with your code
 while True:
@@ -13,7 +16,31 @@ while True:
         print("You will exit.")
         break
 
-    elif len(tokens) < 2:
-        print("Not enough inputs.")
-        continue
+    # elif len(tokens) < 2:
+    #     print("Not enough inputs.")
+    #     continue
     
+    elif tokens[0] == '+':
+        print(add(float(tokens[1]), float(tokens[2])))
+    
+    elif tokens[0] == '-':
+        print(subtract(float(tokens[1]), float(tokens[2])))
+
+    elif tokens[0] == '*':
+        print(multiply(float(tokens[1]), float(tokens[2])))
+    
+    elif tokens[0] == '/':
+        print(divide(float(tokens[1]), float(tokens[2])))
+    
+    elif tokens[0] == 'mod':
+        print(mod(float(tokens[1]), float(tokens[2])))
+
+    elif tokens[0] == 'square':
+        print(square(float(tokens[1])))
+
+    elif tokens[0] == 'cube':
+        print(cube(float(tokens[1])))
+
+    elif tokens[0] == 'pow':
+        print(power(float(tokens[1]), float(tokens[2])))
+
